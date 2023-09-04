ec0d9ea441a457bb0aaaac1a5cb59d8930ad01d8
vipin sharma <vipinrdsocial@gmail.com>
2023-09-04 18:04:18 +0530

    remove lorem ipsum

--- components/valuation/ValuationServices.js
+++ components/valuation/ValuationServices.js
@@ -1,73 +1,99 @@
 import React from 'react';
+
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import PopConvert from '../leadPages/popConvert';
 
-const Slide = ({ title, detail, profileimg, buttonLabel }) => (
-  <div className="slide3">
-     <img src={profileimg} alt="Profile" />
-    <h2>{title}</h2>
-    <p>{detail}</p>
-    <button className='card_btn_sli'>{buttonLabel}</button>
-  </div>
-);
+class ValuationServices extends React.Component {
 
-const ValuationServices = () => {
-  const settings = {
-    dots: false,
-    arrows: true, // Use 'arrows' instead of 'arrow'
-    infinite: true,
-    speed: 500,
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    autoplay: true,
-    responsive: [
-      {
-        breakpoint: 768, // Mobile breakpoint
-        settings: {
-          slidesToShow: 1,
-        },
-      },
-    ],
-    responsive: [
-      {
-        breakpoint: 992, // Mobile breakpoint
-        settings: {
-          slidesToShow: 1,
-        },
-      },
-    ],
-  };
-
-
-  const slidesData = [
-    {
-      profileimg: '/static/images/icons/valutaion2.png',
-      title: 'Lorem ipsum dolor sit.',
-      detail:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid nostrum eligendi rem sequi suscipit ad totam, minima animi inventore maxime iste dolores possimus laborum tempora?',
-      
-        buttonLabel: 'Find more ',
-    },
-    {
-      profileimg: '/static/images/icons/valutaion2.png',
-      title: 'Lorem ipsum dolor sit.',
-      detail:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid nostrum eligendi rem sequi suscipit ad totam, minima animi inventore maxime iste dolores possimus laborum tempora?',
-      
-        buttonLabel: 'Find more ',
-    },
-    
-    // Add more slides as needed
-  ];
-
-  return (
-    <Slider {...settings}>
-      {slidesData.map((slide, index) => (
-        <Slide key={index} {...slide} />
-      ))}
-    </Slider>
-  );
+  state = {
+    'showPopConvert': false
 };
+
+togglePopConvert = () => {
+    this.setState({
+        'showPopConvert': !this.state.showPopConvert
+    })
+}
+
+
+    render() {
+   
+      
+        const settings = {
+          dots: false,
+          arrows: true, // Use 'arrows' instead of 'arrow'
+          infinite: true,
+          speed: 500,
+          slidesToShow: 5,
+          slidesToScroll: 1,
+          autoplay: true,
+          responsive: [
+            {
+              breakpoint: 768, // Mobile breakpoint
+              settings: {
+                slidesToShow: 1,
+              },
+            },
+          ],
+          responsive: [
+            {
+              breakpoint: 992, // Mobile breakpoint
+              settings: {
+                slidesToShow: 1,
+              },
+            },
+          ],
+        };
+      
+      
+        const slidesData = [
+          {
+            profileimg: '/static/images/icons/valutaion2.png',
+            title: 'Lorem ipsum dolor sit.',
+            detail:
+              "Starters' CFO differs from the competition in the simple online processes it has built to help you achieve your valuation needs.",
+            
+              buttonLabel: 'Find more ',
+          },
+          {
+            profileimg: '/static/images/icons/valutaion2.png',
+            title: 'Lorem ipsum dolor sit.',
+            detail:
+              'You can stop struggling through lists of email attachments sent back and forth for ever! Our digital partners include Zoho Books, Tally, Quickbooks & Razorpay amongst others.        ',
+            
+              buttonLabel: 'Find more ',
+          },
+          
+          // Add more slides as needed
+        ];
+      
+    
+        return (
+          
+            <>
+            <PopConvert show={this.state.showPopConvert} onHide={this.togglePopConvert}></PopConvert>
+
+            <Slider {...settings}>
+          
+                {slidesData.map((slide, index) => (
+                  <>
+
+                      <div key={index} className="slide3">
+                      <img src={slide.profileimg} alt="Profile" />
+                      {/* <h2>{title}</h2> */}
+                      <p>{slide.detail}</p>
+                      <button onClick={this.togglePopConvert} className='card_btn_sli'>{slide.buttonLabel}</button>
+                    </div>
+
+                  </>
+                ))}
+            </Slider>
+            </>
+        )
+    }
+}
+
 
 export default ValuationServices;
